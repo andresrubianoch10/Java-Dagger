3072a2c620ea108395b83155027e8baab12986ce
GLOBANT\andres.rubiano <andres.rubiano@globant.com>
2018-05-09 22:21:22 -0500

    Added Motor component inject to let activity using object instance with inject annotations

--- app/src/main/java/com/example/andresrubiano/daggertest/MainActivity.java
+++ app/src/main/java/com/example/andresrubiano/daggertest/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.andresrubiano.daggertest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.andresrubiano.daggertest.di.MotorApplication;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Named("diesel")
+    @Inject
+    Motor motor;
+
+    @Inject
+    Coche coche;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((MotorApplication) getApplication()).getMotorComponent().inject(this);
+        Toast.makeText(this, motor.getTipoMotor(), Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, coche.getMotor(), Toast.LENGTH_SHORT).show();
     }
 }
