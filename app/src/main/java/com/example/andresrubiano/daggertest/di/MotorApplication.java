6ada7d84a9656ab0e9b45f1eab4fa673c51bc2f2
GLOBANT\andres.rubiano <andres.rubiano@globant.com>
2018-05-09 22:19:45 -0500

    Added component builder to create an instance of MotorModule

---
+++ app/src/main/java/com/example/andresrubiano/daggertest/di/MotorApplication.java
@@ -0,0 +1,22 @@
+package com.example.andresrubiano.daggertest.di;
+
+import android.app.Application;
+
+/**
+ * Created by andres.rubiano on 6/05/2018.
+ */
+
+public class MotorApplication extends Application {
+    private MotorComponent motorComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        motorComponent = DaggerMotorComponent.builder().motorModule(new MotorModule()).build();
+    }
+
+    public MotorComponent getMotorComponent() {
+        return motorComponent;
+    }
+}
